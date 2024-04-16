c749789be47ed323a8a7eb3e4df995eb4de6287f
theninexl <thenine@thenine.es>
2024-04-16 16:28:44 +0200

    components architecture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import styles from "./page.module.css";
+import { Description } from "app/components/home/Description";
+import { Hero } from "app/components/home/Hero";
+import { MainProducts } from "app/components/home/MainProducts";
 
 export default function Home() {
 
-  console.log('hola mundo pagina de inicio');
-
   return (
-    <main className={styles.main}>
-      <h1>Hola Mundo</h1>
+    <main>
+      <Hero />
+      <Description />
+      <MainProducts />
     </main>
   );
 }
---
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -0,0 +1,5 @@
+export const MainProducts = () => {
+  return (
+    <p>Main Products</p>
+  )
+}
