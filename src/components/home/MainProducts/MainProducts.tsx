cb83d2d170402b060417d637aa4049a497c534fb
theninexl <thenine@thenine.es>
2024-04-17 17:40:02 +0200

    fetching products from shopify API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,8 +1,18 @@
-"use client"
 
-export const MainProducts = () => {
+const getProducts = async () => {
+  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-04/products.json`, {
+    headers: {
+      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY as string,
+    }
+  })
+  const data = await response.json();
+  return data
+}
 
-  console.log("variable entorno", process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
+
+export const MainProducts = async () => {
+  const products = await getProducts();
+  console.log("products", products)
 
   return (
     <p>Main Products</p>
