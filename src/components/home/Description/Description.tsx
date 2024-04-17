671a4dd03bcdc69edfaa2f258cce5a7e1fe52625
theninexl <thenine@thenine.es>
2024-04-17 11:05:27 +0200

    using Next Image component instead of img

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,16 @@
 import styles from './Description.module.sass'
+import Image from 'next/image'
 
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <img src='/images/description.jpeg' alt='products marketplace' />
+      <Image 
+        src='/images/description.jpeg' 
+        alt='products marketplace'
+        width={500}
+        height={300}
+        priority={false} 
+        quality={30} />
       <div className={styles.Description__text}>
         <h2>Description</h2>
         <p>Lorem ipsum sit dolor amet</p>
