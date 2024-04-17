c5bdf286bfed53221d88e36cfd452fddcc4da847
theninexl <thenine@thenine.es>
2024-04-17 10:50:09 +0200

    adding global static files

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,13 @@
+import styles from './Description.module.sass'
+
 export const Description = () => {
   return (
-    <section>
-      <h1>Descripción</h1>
+    <section className={styles.Description}>
+      <img src='/images/description.jpeg' alt='products marketplace' />
+      <div className={styles.Description__text}>
+        <h2>Description</h2>
+        <p>Lorem ipsum sit dolor amet</p>
+      </div>
     </section>
   )
 }
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import styles from './Header.module.css'
 
 export const Header = () => {
   //este console log saldrá en la consola del navegador y no en el terminal porque el componente padre que es root layout tsx si que tiene la directiva 'use client' implementada
@@ -6,7 +7,7 @@ export const Header = () => {
   return (
     <header>
       <nav>
-        <ul>
+        <ul className={styles.Header__list}>
           <li><Link href='/'>Home</Link></li>
           <li><Link href='/store'>Store</Link></li>
         </ul>
