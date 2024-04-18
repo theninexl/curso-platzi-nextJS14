c4d484c20a40fad7c4cf0c64e72331f3394b2bfc
theninexl <thenine@thenine.es>
2024-04-18 09:56:45 +0200

    route groups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import { Roboto } from 'next/font/google'
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
-import { Hero } from 'app/components/home/Hero';
-import { Description } from 'app/components/home/Description';
 import "app/sass/globals.sass"
 
 const roboto = Roboto({
@@ -19,8 +17,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Header />
-        <Hero />
-        <Description />
         {children}
         <Footer />
       </body>
